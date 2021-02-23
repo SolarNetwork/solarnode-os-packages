4be4fcc3a09208b6ea74547fe107d1aad7d5bdb6
Matt Magoffin <matt@solarnetwork.net>
2021-02-23 17:33:00 +1300

    Update to work with /boot/wpa_supplicant.conf and /boot/firmware/wpa_supplicant.conf, in the Debian Pi image.

---
+++ wifi/debian/usr/share/solarnode/bin/wifi-confcopy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+#
+# Helper script for SolarNode OS WiFi configuration support. This script will
+# move a wpa_supplicant.conf file to the right place.
+
+export WPA_CONF="/etc/wpa_supplicant/wpa_supplicant-wlan0.conf"
+
+if [ -e "$1" ]; then
+	/bin/mv -f "$1" "$WPA_CONF"
+	/bin/chown root:root "$WPA_CONF"
+	/bin/chmod 600 "$WPA_CONF"
+	/usr/sbin/rfkill unblock wifi
+	/usr/share/solarnode/bin/wifi-confsync.sh
+fi
