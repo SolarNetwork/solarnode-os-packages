25608c04165ec3efac06b2423ed29f1dec399061
Matt Magoffin <matt@solarnetwork.net>
2021-09-02 15:09:33 +1200

    Tweak to get working under arm64 Debian 11.

--- pi-usb-support/debian/usr/share/solarnode/bin/pi-tty-usb-namer.sh
+++ pi-usb-support/debian/usr/share/solarnode/bin/pi-tty-usb-namer.sh
@@ -12,7 +12,7 @@
 # named `ttyUSB_X` where `X` is a number staring a 1. The hardware maps the ports like this,
 # looking at the USB ports on the device:
 #
-# Pi 3B, 3B+
+# Pi 3B, 3B+, 4B
 #
 # +-----+ +-----+
 # |  1  | |  3  |
@@ -20,8 +20,7 @@
 # |  2  | |  4  |
 # +-----+ +-----+
 
-# awk call adapted from https://elinux.org/RPi_HardwareHistory#Which_Pi_have_I_got.3F
-CPU=$(awk '/^Revision/ {sub("^1000", "", $3); print $3}' /proc/cpuinfo)
+CPU="$(cat /proc/device-tree/system/linux,revision |xxd -p |sed -e 's/^0*//;s/^[ \t]*//;s/[ \t]*$//')"
 DEVPATH="$1"
 
 if [ -z "$DEVPATH" ]; then
@@ -29,11 +28,11 @@ if [ -z "$DEVPATH" ]; then
 	exit 1
 fi
             
-unknown_dev () {            
+unknown_dev () {
         echo "ttyUSB_$DEVPATH"  
-}                      
- 
-pi3b () {        
+}
+
+pi3b () {
         case "$DEVPATH" in
                 1.2) echo ttyUSB_1 ;;
                 1.3) echo ttyUSB_2 ;;
@@ -41,9 +40,9 @@ pi3b () {
                 1.5) echo ttyUSB_4 ;;
                 *)   unknown_dev ;;
         esac                         
-}                                    
-                                     
-pi3b_plus () {                       
+}
+
+pi3b_plus () {
         case "$DEVPATH" in           
                 1.1.2) echo ttyUSB_1 ;;
                 1.1.3) echo ttyUSB_2 ;;
@@ -51,10 +50,21 @@ pi3b_plus () {
                 1.2)   echo ttyUSB_4 ;;
                 *)     unknown_dev ;;
         esac                         
-}                                    
-                                     
+}
+    
+pi4 () {
+        case "$DEVPATH" in
+                1.2) echo ttyUSB_1 ;;
+                1.1) echo ttyUSB_2 ;;
+                1.4) echo ttyUSB_3 ;;
+                1.3) echo ttyUSB_4 ;;
+                *)   unknown_dev ;;
+        esac                         
+}
+    
 case "$CPU" in
-        a02082|a22082|a32082) pi3b ;;
-        a020d3)               pi3b_plus ;;         
-        *)                    unknown_dev ;;   
+        a02082|a22082|a32082)                                    pi3b ;;
+        a020d3)                                                  pi3b_plus ;;
+        a03111|b03111|b03112|b03114|c03111|c03112|c03114|d03114) pi4 ;; 
+        *)                    unknown_dev ;;
 esac                     
