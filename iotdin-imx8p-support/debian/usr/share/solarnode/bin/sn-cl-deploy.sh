a3ab3d804c8b3cc191a997dd31719fa86ac7e704
Matt Magoffin <matt@solarnetwork.net>
2026-08-10 17:40:15 +1200

    Add sn-cl-deploy helper.

---
+++ iotdin-imx8p-support/debian/usr/share/solarnode/bin/sn-cl-deploy.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env sh
+set -e
+
+cat <<- EOF
+	===============================================================================
+	This will reset this SolarNode, DISCARDING any custom SolarNode settings in
+	this running OS, and copy the OS to the built-in eMMC. This will completely
+	REPLACE the OS on the eMMC.
+
+	If you choose to proceed, when finally prompted if you would like reboot the
+	device, choose NO and instead run the following command so you can safely
+	remove this media before power-cycling the device:
+
+		sudo shutdown -h now
+	===============================================================================
+
+EOF
+
+read -r -p "Are you sure you want to continue? (y/n) " REPLY
+if [ "$REPLY" = 'y' -o "$REPLY" = 'Y' ]]; then
+	# 1: reset SolarNode to clean state
+	sn-reset -a && sudo systemctl stop solarnode
+
+	# 2: Remove SSH host keys
+	sudo rm -f /etc/ssh/ssh_host*
+
+	# 3: Ensure /boot/grub directory exists
+	[ -d /boot/grub ] || sudo mkdir /boot/grub
+
+	# 4: Copy image to eMMC
+	sudo cl-deploy
+fi
